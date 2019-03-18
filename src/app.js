439eae750bf3f6267ad2856689afd9f3170be3e8
Bob Firestone <bob@neonruby.com>
2019-03-18 13:32:15 -0600

    rm seed expenses

--- src/app.js
+++ src/app.js
@@ -12,35 +12,6 @@ import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
 
-// addExpense -> water bill
-store.dispatch(addExpense({ description: "water bill", amount: 2222 }));
-
-// addExpense -> gas bill
-store.dispatch(
-  addExpense({ description: "gas bill", amount: 3333, createdAt: 1000 })
-);
-
-// addExpense -> rent
-store.dispatch(addExpense({ description: "rent", amount: 99999 }));
-
-// setTextFilter -> bill 2 (items) -> water (1 item)
-// store.dispatch(setTextFilter("bill"));
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter("rent"));
-// }, 4000);
-
-// setTimeout(() => {
-//   store.dispatch(setTextFilter());
-// }, 5000);
-
-// getVisableExpenses -> print visable ones to the screen
-// console.log(store.getState());
-// const state = store.getState();
-// console.log(visableExpenses(state.expenses, state.filters));
-
-// console.log(store.getState());
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
